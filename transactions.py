dad39b287dea056101b47c0ded469ec8ed55c1ca
Mitali Prakash Juvekar <mitalijuvekar@Mitalis-MacBook-Air.local>
2024-04-20 01:46:51 -0400

    Add transaction class.

---
+++ transactions.py
@@ -0,0 +1,8 @@
+import time
+
+class Transactions:
+    def __init__(self, sender, recipient, amount):
+        self. sender = sender
+        self.recipient = recipient
+        self. amount = amount
+        self timestamp = time.time()
